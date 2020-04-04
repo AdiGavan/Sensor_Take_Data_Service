6b0c03ba216515a6fd4ee0ac5c82ade4d9268d7c
AdiGavan <adi.gavan@gmail.com>
2020-04-04 21:52:23 +0300

    Lower case json parameters

--- Sensor_Take_Data_Service.py
+++ Sensor_Take_Data_Service.py
@@ -35,10 +35,15 @@ def addDataToDatabase(sensorType, sensorDate, sensorTime, sensorValue):
     global db
     db = psycopg2.connect(host='db_sensors', port=5432, user='postgres', password='postgres', dbname='sensors_info_db')
     cursor = db.cursor()
-    cursor.execute("INSERT INTO sensors_values(sensor_type, sensor_date, sensor_time, sensor_value) VALUES(%s, %s, %s, %s) RETURNING id", (sensorType, sensorDate, sensorTime, sensorValue))
-    lineID = cursor.fetchone()[0]
-    cursor.close()
-    db.commit()
+    try:
+        cursor.execute("INSERT INTO sensors_values(sensor_type, sensor_date, sensor_time, sensor_value) VALUES(%s, %s, %s, %s) RETURNING id", (sensorType, sensorDate, sensorTime, sensorValue))
+        lineID = cursor.fetchone()[0]
+        db.commit()
+    except:
+        lineID = -1
+
+    if cursor is not None:
+        cursor.close()
     if db is not None:
         db.close()
 
@@ -49,14 +54,24 @@ def addDataToDatabase(sensorType, sensorDate, sensorTime, sensorValue):
 def take_data():
     
     jsonData = request.get_json()
-    sensorType = jsonData['SensorType']
-    sensorDate = jsonData['SensorDate']
-    sensorTime = jsonData['SensorTime']
-    sensorValue = jsonData['SensorValue']
+    sensorType = jsonData['sensortype']
+    sensorDate = jsonData['sensordate']
+    sensorTime = jsonData['sensortime']
+    sensorValue = jsonData['sensorvalue']
     lineID = addDataToDatabase(sensorType, sensorDate, sensorTime, sensorValue)
 
     # Test (returneaza datele introduse + id-ul)
-    return jsonify({'result' : 'Success!', 'Type' : sensorType, 'Date' : sensorDate, "Time" : sensorTime, "Value" : sensorValue, "ID" : lineID})
+    #return jsonify({'result' : 'Success!', 'Type' : sensorType, 'Date' : sensorDate, "Time" : sensorTime, "Value" : sensorValue, "ID" : lineID})
+    
+    if lineID == -1:
+        status = "Failed"
+        error = "Error at adding the line in the database."
+
+    else:
+        status = "Success"
+        error = "Nothing"
+
+    return jsonify({"status" : status, "error" : error})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
